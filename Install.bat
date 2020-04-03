ccc2e63359b155c29a062b6411d2ec34d6e39b24
mchaptel <mchaptel@xilam.com>
2020-04-03 22:32:17 +0200

    new install.bat

--- Install.bat
+++ Install.bat
@@ -3,49 +3,37 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 SET dlPath=%~dp0
 set harmonyPrefsDir=%appdata%\Toon Boom Animation
 
-echo -- Starting install of openHarmony open source scripting library --
+SETX LIB_OPENHARMONY_PATH %dlPath%
 
-rem Check Harmony Versions and make a list
-set i=0
-for /d %%D in ("%harmonyPrefsDir%\*") do (
+echo -------------------------------------------------------------------
+echo -- Starting install of openHarmony open source scripting library --
+echo -------------------------------------------------------------------
+echo OpenHarmony will be installed to the folder :
+echo %dlpath%
+echo Do not delete the contents of this folder.
+
+REM Check Harmony Versions and make a list
+for /d %%D in ("%harmonyPrefsDir%\*Harmony*") do (
   set harmonyVersionDir=%%~fD
   for /d %%V in ("!harmonyVersionDir!\*-layouts*") do (
-    set /a i+=1
     set "folderName=%%~nD"
     set "versionName=%%~nV"
-    set "harmonyFolder[!i!]=!folderName:~-7!"
-    set "harmonyVersions[!i!]=!versionName:~0,2!"
-    set versionString[!i!]=Harmony !versionName:~0,2! !folderName:~-7!
+    set "harmonyFolder=!folderName:~-7!"
+    set "harmonyVersions=!versionName:~0,2!"
+    echo Found Toonboom Harmony !harmonyFolder! !harmonyVersions! - installing openHarmony for this version.
+    set "installDir=!harmonyPrefsDir!\Toon Boom Harmony !harmonyFolder!\!harmonyVersions!00-scripts\"
+
+    REM create script folder if missing
+    if not exist "!installDir!" mkdir "!installDir!"
+
+    cd !installDir!
+    
+    set "script=include(System.getenv('LIB_OPENHARMONY_PATH')+'openHarmony.js');" 
+    
+    echo !script!> openHarmony.js
+    echo ---- done. ----
   )
 )
-
-rem offer choice if more than one version
-if %i% GEQ 2 (
-  rem print out the list
-  echo Found %i% installed Toonboom Harmony versions:
-  for /l %%a in (1 1 %i%) do ( echo %%a. !versionString[%%a]! )
-  set /p choice=Install OpenHarmony for which Harmony version? : 
-) else (
-  set choice=1
-)
-
-set "installDir=%harmonyPrefsDir%\Toon Boom Harmony !harmonyFolder[%choice%]!\!harmonyVersions[%choice%]!00-scripts\"
-echo Installing openHarmony to directory :
-echo %installDir%
-
-rem create script folder if missing
-if not exist "%installDir%" mkdir "%installDir%"
-
-rem copy scriptfiles into destination
-(echo .bat 
-echo .json
-echo .txt
-echo .gitignore
-echo .gitattributes
-echo .md)> exclude.txt
-
-xcopy "%dlPath%*" "%installDir%" /y /s /i /e /q /exclude:exclude.txt
-del exclude.txt
 
 echo - Install Complete -
 pause
