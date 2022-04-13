808325d705311d6cc8aa47ebc364b8a28806c32f
waterwheels <joe.lombard@me.com>
2022-04-13 16:56:19 +1000

    Update openHarmony_dialog.js

--- openHarmony/openHarmony_dialog.js
+++ openHarmony/openHarmony_dialog.js
@@ -255,7 +255,7 @@ $.oDialog.prototype.prompt = function( labelText, title, prefilledText){
  * @param   {string}           [acceptMultiple=false]        Whether or not selecting more than one file is ok. Is ignored if getExisting is falses.
  * @param   {string}           [startDirectory]              The directory showed at the opening of the dialog.
  *
- * @return  {oFile[]}         The list of oFile objects, 'undefined' if the dialog is cancelled
+ * @return  {string[]}         The list of selected Files, 'undefined' if the dialog is cancelled
  */
 $.oDialog.prototype.browseForFile = function( text, filter, getExisting, acceptMultiple, startDirectory){
   if (this.$.batchMode) {
@@ -279,12 +279,8 @@ $.oDialog.prototype.browseForFile = function( text, filter, getExisting, acceptM
     var _files = QFileDialog.getSaveFileName(0, text, startDirectory, filter);
   }
 
-  if (Array.isArray(_files)) {
-    for (var i = 0; i < _files.length; i++) {
-      _files[i] = new oFile(_files[i].replace(/\\/g, "/"));
-    }
-  } else {
-    _files = new oFile(_files.replace(/\\/g, "/"));
+  for (var i in _files){
+    _files[i] = _files[i].replace(/\\/g, "/");
   }
 
   this.$.debug(_files);
@@ -297,7 +293,7 @@ $.oDialog.prototype.browseForFile = function( text, filter, getExisting, acceptM
  * @param   {string}           [text]                        The title of the confirmation dialog.
  * @param   {string}           [startDirectory]              The directory showed at the opening of the dialog.
  *
- * @return  {oFolder}          An oFolder object for the selected folder, 'undefined' if the dialog is cancelled
+ * @return  {string}           The path of the selected folder, 'undefined' if the dialog is cancelled
  */
 $.oDialog.prototype.browseForFolder = function(text, startDirectory){
   if (this.$.batchMode) {
@@ -310,7 +306,6 @@ $.oDialog.prototype.browseForFolder = function(text, startDirectory){
   var _folder = QFileDialog.getExistingDirectory(0, text, startDirectory);
   _folder = _folder.split("\\").join("/");
   // this.$.alert(_folder)
-  _folder = new oFolder(_folder);
   return _folder;
 }
 
@@ -545,7 +540,7 @@ function openMenu(){
 
   onionSkinSlider = new QSlider(Qt.Horizontal)
   onionSkinSlider.minimum = 0;
-	onionSkinSlider.maximum = 256;
+  onionSkinSlider.maximum = 256;
   onionSkinSlider.valueChanged.connect(function(value){
     preferences.setDouble("DRAWING_ONIONSKIN_MAX_OPACITY",
       value/256.0);
