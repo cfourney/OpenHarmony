78b8a7891b9edcda68603d94adcd983767b92526
MathieuC <m.chaptel@gmail.com>
2022-07-15 18:12:25 +0200

    implement support for timing nodes in timingColumn

--- openHarmony/openHarmony_node.js
+++ openHarmony/openHarmony_node.js
@@ -1784,7 +1784,7 @@ $.oNode.prototype.getAttributesColumnCache = function( obj_lut ){
  * @param   {oNode}         nodeToLink                          The target node as an in node.
  * @param   {int}           ownPort                             The out port on this node to connect to.
  * @param   {int}           destPort                            The in port on the inNode to connect to.
- *
+ * @deprecated use $.oNode.linkOutNode() instead
  * @return {$.oNodeLink}    the resulting created link.
  * @example
  *  var peg1     = $.scene.getNodeByPath( "Top/Peg1" );
@@ -1968,12 +1968,12 @@ $.oDrawingNode.prototype.constructor = $.oDrawingNode;
  */
 Object.defineProperty($.oDrawingNode.prototype, "element", {
   get : function(){
-    var _column = this.attributes.drawing.element.column;
+    var _column = this.timingColumn;
     return ( new this.$.oElement( node.getElementId(this.path), _column ) );
   },
 
   set : function( oElementObject ){
-    var _column = this.attributes.drawing.element.column;
+    var _column = this.timingColumn;
     column.setElementIdOfDrawing( _column.uniqueName, oElementObject.id );
   }
 });
@@ -1986,13 +1986,17 @@ Object.defineProperty($.oDrawingNode.prototype, "element", {
  */
 Object.defineProperty($.oDrawingNode.prototype, "timingColumn", {
   get : function(){
-    var _column = this.attributes.drawing.element.column;
+    var _isTimingNode = this.drawing.element_mode;
+    var _attr = _isTimingNode ? this.attributes.drawing.element : this.attributes.drawing.custom_name.timing;
+    var _column = _attr.column;
     return _column;
   },
 
   set : function (oColumnObject){
-    var _attribute = this.attributes.drawing.element;
-    _attribute.column = oColumnObject;
+    var _isTimingNode = this.drawing.element_mode;
+    var _attr = _isTimingNode?this.attributes.drawing.element:this.attributes.drawing.custom_name.timing;
+
+    _attr.column = oColumnObject;
   }
 });
 
@@ -2080,7 +2084,7 @@ Object.defineProperty($.oDrawingNode.prototype, "usedColors", {
  */
 Object.defineProperty($.oDrawingNode.prototype, "timings", {
     get : function(){
-        return this.attributes.drawing.element.getKeyframes();
+        return this.timingColumn.getKeyframes();
     }
 })
 
@@ -2108,8 +2112,7 @@ Object.defineProperty($.oDrawingNode.prototype, "palettes", {
 $.oDrawingNode.prototype.getDrawingAtFrame = function(frameNumber){
   if (typeof frame === "undefined") var frame = this.$.scene.currentFrame;
 
-  var _attribute = this.attributes.drawing.element
-  return _attribute.getValue(frameNumber);
+  return this.timingColumn.getValue(frameNumber);
 }
 
 
@@ -3719,7 +3722,7 @@ $.oGroupNode.prototype.importQT = function( path, importSound, extendScene, alig
   var _elementName = _element.name;
 
   var _movieNode = this.addDrawingNode(_movieName, nodePosition, _element);
-  var _column = _movieNode.attributes.drawing.element.column;
+  var _column = _movieNode.timingColumn;
   _element.column = _column;
 
   // setup the node
