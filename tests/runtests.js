d63255225e0f9b0cb90f5d5a753db4c64d6f07bb
mchaptel <m.chaptel@gmail.com>
2025-09-18 18:15:36 +0700

    adding some tests and improving the reporting system

--- tests/runtests.js
+++ tests/runtests.js
@@ -1,8 +1,9 @@
 function run() {
   var tests = require("./tests.js");
-  for (var i in tests){
-    tests[i].execute()
+  for (var i in tests.tests){
+    tests.tests[i].execute()
   }
+  tests.reportErrors();
 }
 
 exports.run = run
