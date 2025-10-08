4848ae12d80bd2b40854484ff428718747348045
mchaptel <m.chaptel@gmail.com>
2025-10-08 18:38:16 +0700

    refactor currentTool getter/setter now returns oTool (from drawing additions branch)

--- openHarmony/classes/oApp.js
+++ openHarmony/classes/oApp.js
@@ -194,31 +194,31 @@ Object.defineProperty(oApp.prototype, 'tools', {
 
 
 /**
- * The Position of the mouse cursor in the screen coordinates.
+ * The currently active Tool.
  * @name $.oApp#currentTool
  * @type {$.oTool}
  */
 Object.defineProperty(oApp.prototype, 'currentTool', {
   get : function(){
     var _tool = Tools.getToolSettings().currentTool.id;
-    return _tool;
+    return this.tools[_tool];
   },
   set : function(tool){
-    if (tool instanceof this.$.oTool) {
-      tool.activate();
-      return
-    }
-    if (typeof tool == "string"){
-      try{
-        this.getToolByName(tool).activate();
-        return
-      }catch(err){
-        this.$.debug("'"+ tool + "' is not a valid tool name. Valid: "+this.tools.map(function(x){return x.name}).join(", "))
+    try{
+      switch(typeof tool){
+        case "string":
+          this.getToolByName(tool).activate();
+          break;
+
+        case "number":
+          this.tools[tool].activate();
+          break;
+
+        default:
+          if (tool instanceof this.$.oTool) tool.activate();
       }
-    }
-    if (typeof tool == "number"){
-      this.tools[tool].activate();
-      return
+    }catch(err){
+      this.$.debug("'"+ tool + "' is not a valid tool. Valid: "+this.tools.map(function(x){return x.name}).join(", "))
     }
   }
 });
@@ -448,5 +448,28 @@ oApp.prototype.runMenuCommand = function(menuName, menuString){
     }
   }
 }
+
+
+/**
+ * Some actions are only available while certain tools are active. Use this function to execute a custom function after setting a given tool active.
+ * @param {$.oTool}     tool      the tool object we want to use (also support tool names as string)
+ * @param {function}    func      the function to execute
+ * @param {object}      context   the context in which the function will execute
+ * @param {object[]}    arguments a list of arguments to pass to the function
+ * @return {}  return whatever the function passed returned.
+ */
+ oApp.prototype.doWithTool = function (tool, func, context, args){
+  if (typeof context === 'undefined') context = func;
+  if (typeof args === 'undefined') args = [];
+
+  var _curTool = this.currentTool;
+  this.currentTool = tool;
+  var value = func.apply(context, args);
+  this.currentTool = _curTool;
+
+  return value;
+}
+
+
 
 exports.oApp = oApp;
